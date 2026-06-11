575e5ad95f00a80882397abc6e120c4d3bb106c1
Martin Lamoureux <martinlamoureux23@gmail.com>
2026-06-11 12:42:44 -0400

    fuck this shit

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,30 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="page">
+    <h1>Fuck this shit, call me to purchase domain.</h1>
+  </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: #000;
+}
+
+h1 {
+  color: #fff;
+  font-family: system-ui, sans-serif;
+  font-size: clamp(2rem, 8vw, 6rem);
+  font-weight: 900;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+}
+</style>
